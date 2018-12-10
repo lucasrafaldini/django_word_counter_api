ece6817c0f857beecc472f9e17a84f024a1fe081
lucasrafaldini <lucas.rafaldini@gmail.com>
2018-12-10 13:03:29 -0200

    AGORA VAI

--- django_word_counter_api/urls.py
+++ django_word_counter_api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.decorators.cache import cache_page
-
+from django.views.generic.base import RedirectView
 
 app_name = 'api'
 
